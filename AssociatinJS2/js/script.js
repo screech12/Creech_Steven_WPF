1bf42e743e42060faf0b7f6e37df74653bfc0ba6
steven creech <screech12@fullsail.edu>
2013-10-08 16:08:57 -0400

    class assignment
    Class assignments practise

---
+++ AssociatinJS2/js/script.js
@@ -0,0 +1,4 @@
+/**
+ * Created by stevencreech on 10/7/13.
+ */
+alert("test");
---
+++ AssociatingJS/js/script.js
@@ -0,0 +1,6 @@
+alert("Hello World!");
+// JavaScript
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF00";
+
