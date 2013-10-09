b29b769cb4b622c81be8a804fb2a33abf343b740
steven creech <screech12@fullsail.edu>
2013-10-09 15:36:51 -0400

    added Pizza problem
    added Slice of Pie part 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -8,8 +8,9 @@ console.log(age);
 
 //How many slices of pizza does each person receive.
 
-var pizza = a = 16;//number of Pizza
-//number of people
-//number of pizza ordered
-//slices per person
-console.log();
+var pizzaSlices = 16;//number of Pizza Slices
+var people = 5;//number of people
+var numberOfPie = 2//number of pizza ordered
+var slices = numberOfPie * pizzaSlices / people; //slices per person
+console.log("Slice of Pie part 1")
+console.log(slices);
