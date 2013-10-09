b4bfb434e0087d28ac01bdb921057d9871ee328b
steven creech <screech12@fullsail.edu>
2013-10-09 15:00:42 -0400

    setup assignment Expression
    setup of Expression of Dog Age.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,15 @@
+//Steven Creech Oct. 09, 2013 Expressions_worksheet
+//Calculate the actual age of a dog years into human years
+
+var dogAge = 1//Age of dog
+var age = 7 * dogAge;//times 7 to a human years to dog years
+console.log("Dog Years");
+console.log(age);
+
+//How many slices of pizza does each person receive.
+
+var pizza = a = 16;//number of Pizza
+//number of people
+//number of pizza ordered
+//slices per person
+console.log();
