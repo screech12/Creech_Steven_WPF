4ce0716f59de2046cbf065b686d703ca7f85aa0e
steven creech <screech12@fullsail.edu>
2013-10-10 10:57:18 -0400

    monthly total
    setup total monthly expenses

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,8 +2,12 @@
 
 //How much money do I need each month to pay my bills? For each week? Factor in expenses Rent, Power bill, water.
 
-var userInput = prompt("How much money is need each month and each week. Please enter Rent Expenses");//first question in calculation monthly and week expenses.
+var userInput = prompt("How much money is need each month and each week.\nPlease enter monthly Rent Expenses");//first question in calculation monthly and week expenses.
 var powerBill = prompt("What is your monthly Power Bill?");//Monthly power bill
+var waterBill = prompt("What is your monthly water bill?")//water bill monthly
+var monthlyTotal = userInput + powerBill + waterBill;
 
 console.log(userInput);
 console.log(powerBill);
+console.log(waterBill);
+console.log(monthlyTotal);
