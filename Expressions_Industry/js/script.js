d28d43f49039bd704f9ac50c1474aff74d8bad3d
steven creech <screech12@fullsail.edu>
2013-10-10 15:02:40 -0400

    first prompt
    prompt asking for a value

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,1 +1,7 @@
-Steven Creech October 10, 2013 Expressions_Industry
+//Steven Creech October 10, 2013 Expressions_Industry
+
+// How much money is needed to advertise my business after expanses. with different percentages.
+
+var grossIncome = prompt ("How much can I spend on advertising my business");//Prompted to input a gross value of income
+
+console.log()
