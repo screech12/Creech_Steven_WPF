e730559f0b028db61c1c1b7809c70814261e4501
steven creech <screech12@fullsail.edu>
2013-10-10 16:03:33 -0400

    available amount for advertisement

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,9 +5,10 @@
 var grossIncome = prompt ("How much can I spend on advertising my business. \nPlease enter the amount in the field below.");//Prompted to input a gross value of income
 var businessExpenses = prompt ("Minus all company expenses.");//Total business expenses
 var advertisementPercentage = prompt("Please enter a percentage from 5 to 10 percent")//asking for a percentage to be entered("The following is the dollar amount available for advertisement $")
-
-alert("This is your available amount for advertisement $")//this is the final amount available to advertise with
+var advertisementAmount = (grossIncome - businessExpenses) / (advertisementPercentage);
+alert("This is your available amount for advertisement $" + advertisementAmount)//this is the final amount available to advertise with
 
 console.log(grossIncome + " " + "dollars");//gross business income
 console.log(businessExpenses + " " + "dollars");// business expenses
 console.log(advertisementPercentage + " " + "percentage");// percentage amount
+console.log(advertisementAmount + " " + "dollars");//advertisement amount
