325224bc6bd18b9c61aa4db4bdf0cd4f7d66788a
steven creech <screech12@fullsail.edu>
2013-10-10 15:26:55 -0400

    advertising percentage
    3rd prompt completed and verified

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,8 +3,10 @@
 // How much money is needed to advertise my business after expanses. with different percentages.
 
 var grossIncome = prompt ("How much can I spend on advertising my business. \nPlease enter the amount in the field below.");//Prompted to input a gross value of income
-var businessExpenses = prompt ("Minus all company expenses.");
+var businessExpenses = prompt ("Minus all company expenses.");//Total business expenses
+var advertisementPercentage = prompt("Please enter a percentage from 5 to 10 percent")//asking for a percentage to be entered
 
 
 console.log(grossIncome + " " + "dollars");//gross business income
 console.log(businessExpenses + " " + "dollars");// business expenses
+console.log(advertisementPercentage + " " + "percentage");// percentage amount
