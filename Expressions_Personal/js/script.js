260ed7ff9230093695f9261c40d0cc9a1d806b79
steven creech <screech12@fullsail.edu>
2013-10-10 13:45:40 -0400

    added dollar signs
    corrected issues

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,11 +5,14 @@
 var userInput = prompt("How much money is need each month and each week.\nPlease enter monthly Rent Expenses");//first question in calculation monthly and week expenses.
 var powerBill = prompt("What is your monthly Power Bill?");//Monthly power bill
 var waterBill = prompt("What is your monthly water bill?");//water bill monthly
-var monthlyTotal = parseInt(userInput) + parseInt(powerBill) + parseInt(waterBill);
+var monthlyTotal = parseInt(userInput) + parseInt(powerBill) + parseInt(waterBill);//Total for month
+var weeklyTotal = (parseInt(userInput) + parseInt(powerBill) + parseInt(waterBill))/3;//Total for week
 
-alert("The total of your monthly bills are? " + monthlyTotal + " per month");//total monthly and weekly expenses
+alert("The total of your monthly bills are? $" + monthlyTotal + " per month and weekly $ " + weeklyTotal);//total monthly and weekly expenses
+
 
 console.log(userInput);
 console.log(powerBill);
 console.log(waterBill);
 console.log(monthlyTotal);
+console.log(weeklyTotal);
