68125105dedf3c6d064ba80d43c9f140d527b168
steven creech <screech12@fullsail.edu>
2013-10-10 15:15:57 -0400

    second prompt
    setup prompt for business expenses

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,6 +2,9 @@
 
 // How much money is needed to advertise my business after expanses. with different percentages.
 
-var grossIncome = prompt ("How much can I spend on advertising my business");//Prompted to input a gross value of income
+var grossIncome = prompt ("How much can I spend on advertising my business. \nPlease enter the amount in the field below.");//Prompted to input a gross value of income
+var businessExpenses = prompt ("Minus all company expenses.");
 
-console.log()
+
+console.log(grossIncome + " " + "dollars");//gross business income
+console.log(businessExpenses + " " + "dollars");// business expenses
