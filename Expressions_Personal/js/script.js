fb05ff7a69c74f835535395d792fa6abf49c1bf1
steven creech <screech12@fullsail.edu>
2013-10-10 10:22:37 -0400

    Setting up prompt
    first prompt for rent

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,5 @@
-Steven Creech October 10,2013 Expressions_Personal
+//Steven Creech October 10,2013 Expressions_Personal
+
+//How much money do I need each month to pay my bills? For each week? Factor in expenses Rent, Power bill, water.
+
+prompt("How much money is need each month and each week. Please enter Rent Expenses");
