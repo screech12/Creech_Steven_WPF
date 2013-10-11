4d46194289e766707b32d293b10a35eb24b39b72
steven creech <screech12@fullsail.edu>
2013-10-11 00:20:59 -0400

    corrected addition issue
    add parseInt to correct addition issue

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,8 +2,8 @@
 
 //Create a calculator using javascript and what was leaned.
 
-var a = prompt ("This input will do adding.\n Please enter a number")//input for first number to add
-var b = prompt ("This input will do adding.\n Please enter your second a number")//input for second number to add
+var a = parseInt(prompt ("This input will do Adding.\n Please enter a number"))//input for first number to add
+var b = parseInt(prompt ("This input will do adding.\n Please enter your second a number"))//input for second number to add
 var totalAdding =  a + b;//adding two numbers together
 alert("This is the total of the two numbers you wanted added together. " + totalAdding);
 
