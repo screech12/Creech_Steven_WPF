d2c7a147bb04bc023124eb6cf6c494d5150d690c
steven creech <screech12@fullsail.edu>
2013-10-10 22:52:57 -0400

    setup multiply prompt
    prompt for multiply

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,11 +2,13 @@
 
 //Create a calculator using javascript and what was leaned.
 
-var a = prompt ("This input will do adding.\n Please enter a number")//input for first number to add
-var b = prompt ("This input will do adding.\n Please enter your second a number")//input for second number to add
-var totalAdding =  a + b;//adding two numbers together
-alert("This is the total of the two numbers you wanted added together. " + totalAdding);
+//var a = prompt ("This input will do adding.\n Please enter a number")//input for first number to add
+//var b = prompt ("This input will do adding.\n Please enter your second a number")//input for second number to add
+//var totalAdding =  a + b;//adding two numbers together
+//alert("This is the total of the two numbers you wanted added together. " + totalAdding);
 
-console.log(a + " adding");// first number for adding
-console.log(b + " adding");// first number for adding
-console.log(totalAdding + " adding");// first and second numbers for adding
+//console.log(a + " adding");// first number for adding
+//console.log(b + " adding");// first number for adding
+//console.log(totalAdding + " adding");// first and second numbers for adding
+
+var c = prompt ("This input will multiply to numbers. \nPlease enter a number")
