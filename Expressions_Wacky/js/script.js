e61181785e9355cddf6cdd45d581e8f09138c905
steven creech <screech12@fullsail.edu>
2013-10-10 22:48:24 -0400

    issue with adding numbers
    having issues with adding

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,9 +4,8 @@
 
 var a = prompt ("This input will do adding.\n Please enter a number")//input for first number to add
 var b = prompt ("This input will do adding.\n Please enter your second a number")//input for second number to add
-var totalAdding =  a + + b;//adding two numbers together
-alert("This is the total of the two numbers you wanted added together." + totalAdding);
-
+var totalAdding =  a + b;//adding two numbers together
+alert("This is the total of the two numbers you wanted added together. " + totalAdding);
 
 console.log(a + " adding");// first number for adding
 console.log(b + " adding");// first number for adding
