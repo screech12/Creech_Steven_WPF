ec57f76bfb6a1130a0fb5ab01efd13351cfc6885
steven creech <screech12@fullsail.edu>
2013-10-11 00:04:13 -0400

    title
    titles added to consoles

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,23 +2,25 @@
 
 //Create a calculator using javascript and what was leaned.
 
-//var a = prompt ("This input will do adding.\n Please enter a number")//input for first number to add
-//var b = prompt ("This input will do adding.\n Please enter your second a number")//input for second number to add
-//var totalAdding =  a + b;//adding two numbers together
-//alert("This is the total of the two numbers you wanted added together. " + totalAdding);
+var a = prompt ("This input will do adding.\n Please enter a number")//input for first number to add
+var b = prompt ("This input will do adding.\n Please enter your second a number")//input for second number to add
+var totalAdding =  a + b;//adding two numbers together
+alert("This is the total of the two numbers you wanted added together. " + totalAdding);
 
-//console.log(a + " adding");// first number for adding
-//console.log(b + " adding");// second number for adding
-//console.log(totalAdding + " adding");// first and second numbers for adding
+console.log("Addition");//Title
+console.log(a + " adding");// first number for adding
+console.log(b + " adding");// second number for adding
+console.log(totalAdding + " adding");// first and second numbers for adding
 
-//var c = prompt ("This input will multiply two numbers. \nPlease enter a number")
-//var d = prompt ("This input will multiply two numbers. \nPlease enter second  number")
-//var multiTotal = c * d;
-//alert("This is the total of the two numbers you wanted multiplied. " + multiTotal );
+var c = prompt ("This input will multiply two numbers. \nPlease enter a number")
+var d = prompt ("This input will multiply two numbers. \nPlease enter second  number")
+var multiTotal = c * d;
+alert("This is the total of the two numbers you wanted multiplied. " + multiTotal );
 
-//console.log(c + " multiply");// first number for multiplying
-//console.log(d + " multiply");// second number for multiplying
-//console.log(multiTotal +" multiply");//total of multiplied numbers
+console.log("multiplication");//Title
+console.log(c + " multiply");// first number for multiplying
+console.log(d + " multiply");// second number for multiplying
+console.log(multiTotal +" multiply");//total of multiplied numbers
 
 var e = prompt ("This input will do division.\n Please enter a number")//input for first number to divided by
 var d = prompt("This input will division.\nPlease enter your second number.")//input for second number to be divided by
@@ -26,6 +28,7 @@ var dividTotal = e / d;// total of division
 
 alert("This is the total of the two numbers you wanted divided. " + dividTotal );
 
+console.log("Division");//Title
 console.log(e + " division");// first number for divided by
 console.log(d + " division");// second number for divided by
 console.log(dividTotal +" division");//total of divided numbers
