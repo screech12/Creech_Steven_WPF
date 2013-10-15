bd5e1e4926b5385227d977d33fbe0168398fa0e1
steven creech <screech12@fullsail.edu>
2013-10-15 19:58:23 -0400

    conditional
    setup gas example

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,16 @@
+// Steven_Creech_October_14_2013_Conditionals_worksheet
+
+//Last Chance for Gas!
+//
+//    A driver has to determine if they can make it across the desert with their current fuel.  They are about to past the last gas station for the next 200 miles and they need to determine whether they should stop now for gas or not.
+//
+//    Given:
+//Gas efficiency of the car (in mpg)
+//Gauge reading of the gas tank (in %)
+//Car’s gas tank capacity (in gallons)
+//Result To Print Out:
+//    “Yes, you can make it without stopping for gas!” or “You only have X gallons of gas in your tank, better get gas now while you can!”
+
+var gasMpg = 'Gas efficiency of the car (in mpg)';
+var gaugeReading = 'Gauge reading of the gas tank (in %)';
+var gasCapacity = 'Car’s gas tank capacity (in gallons)';
