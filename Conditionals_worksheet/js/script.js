159b31e0b84264a402c97b983ccf93fa99384893
steven creech <screech12@fullsail.edu>
2013-10-15 20:50:10 -0400

    Movie Ticket Price
    settup 3rd example

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -30,3 +30,14 @@ if(){
 //Grade (in percent)
 //Result To Print Out:
 //    “You have a X%, which means you have earned a(n) X in the class!”
+
+
+//Movie Ticket Price
+//
+//The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
+//    Determine which of the two prices the customer is eligible for.
+//Given:
+//    Time of Movie (Assume whole numbers here)
+//Age of the customer
+//Result To Print Out:
+//    “The ticket price is X”
