8b28b4055052bd5d1ea38b231d2033b4aa9002bf
steven creech <screech12@fullsail.edu>
2013-10-15 20:48:20 -0400

    Grade Letter Calculator
    setting up example2

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -14,3 +14,19 @@
 var gasMpg =  23;//Gas efficiency of the car (in mpg)
 var gaugeReading = 50;//Gauge reading of the gas tank (in %)
 var gasCapacity = 16;//Car’s gas tank capacity (in gallons)
+
+if(){
+    console.log()
+}
+
+
+//Grade Letter Calculator
+//
+//A student earns a number grade at the conclusion of a course at Full Sail.  Determine the appropriate letter grade for that number using conditional statements.
+//    Assume grades are whole numbers that never go below 0 or above 100.
+//Use the FullSail Grade scale.
+//    There should be only one print out to the console.
+//    Given:
+//Grade (in percent)
+//Result To Print Out:
+//    “You have a X%, which means you have earned a(n) X in the class!”
