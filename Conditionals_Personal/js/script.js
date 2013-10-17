f53763da9fee5b7b20f067536802b102030dbafa
steven creech <screech12@fullsail.edu>
2013-10-17 19:17:22 -0400

    issues with adding
    total var added with alert

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,7 +4,11 @@
 //It is the end of the month and rent and power bill is due. There is no income. What do you do first?
 var rent = prompt('Late on your rent? \nEnter your monthly rent due.'); //monthly rent now due
 var power = prompt('Late on Power bill? Please enter monthly amount.');//monthly power bill due
-if(rent){
-    //if rent is due this is true
-    console.log('Talk with your Landlord');
-}
+var total = parseInt (rent + power);//calculates info
+
+alert("Total of bills due is $" + total);
+console.log (total);//prints to the console
+//if(rent){
+//    //if rent is due this is true
+//    console.log('Talk with your Landlord');
+//}
