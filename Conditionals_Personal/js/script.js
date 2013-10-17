f8e2334f2ec0474bdc3da032ca3cc39817c17170
steven creech <screech12@fullsail.edu>
2013-10-17 18:13:34 -0400

    second given
    next var entered with prompt and chked with browser

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,4 +2,5 @@
 
 //Bills
 //It is the end of the month and rent and power bill is due. There is no income. What do you do first?
-var rent = prompt('Late on your rent? \nEnter your monthly rent due'); //monthly rent now due
+var rent = prompt('Late on your rent? \nEnter your monthly rent due.'); //monthly rent now due
+var power = prompt('Late on Power bill? Please enter monthly amount.');
