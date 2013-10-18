38619b98b6e40e5acafc052cda6e4f4fca8198f9
steven creech <screech12@fullsail.edu>
2013-10-18 02:14:08 -0400

    listed the givens
    listed the first var

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,2 +1,6 @@
 //Steven_Creech_October_17_2013_Conditionals_Wacky
 
+//Something wacky
+//How many jelly beans will it take to fill a 16oz jar, Jelly beans it take 20 to make a oz.
+
+var jar = 16;
