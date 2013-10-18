cae7c7ee4fac99e163030416e69742c82318104e
steven creech <screech12@fullsail.edu>
2013-10-17 23:16:36 -0400

    first given
    var prompt with request

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,4 +2,4 @@
 
 //determine how many hours are need to complete a website
 //Given hours per page, how many pages, total time needed
-var hours = prompt(); // hours needed per page
+var hours = prompt('Enter hours in take you to complete a Web page'); // hours needed per page
