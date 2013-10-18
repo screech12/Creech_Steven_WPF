01ada2300b7919b063337e697cf5b07a9a8eaa60
steven creech <screech12@fullsail.edu>
2013-10-18 03:29:46 -0400

    var total
    total and if statement and console created

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,11 +3,13 @@
 //Something wacky
 //How many jelly beans will it take to fill a a pound jar, Jelly beans it take 20 to make a oz.
 
-var jar = prompt('How many jelly beans will fill a pound jar? \nWhats your guess?');// to be filled with jelly beans
+var jar = prompt('How many jelly beans will fill a pound jar? \nWhat is your guess?');// to be filled with jelly beans
 var jellyBean = prompt('A jelly beans is 1/16th of an oz. \nNow what is your guess?');
-var total;
+var total =(jar >= jellyBean);
 
-
+if(total >= 256)// the answer is true
 console.log ('Something wacky');//Title
 console.log(jar + '' + ' each');//print console
 console.log(jellyBean + '' + 'oz');//print jellybean amount
+console.log(total);
+
