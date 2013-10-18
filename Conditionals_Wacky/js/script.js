d78da5515a64dbf639f2682bc3bb80b8e8a6e244
steven creech <screech12@fullsail.edu>
2013-10-18 02:59:44 -0400

    change comment
    jellybean comment change

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,12 +1,12 @@
 //Steven_Creech_October_17_2013_Conditionals_Wacky
 
 //Something wacky
-//How many jelly beans will it take to fill a 16oz jar, Jelly beans it take 20 to make a oz.
+//How many jelly beans will it take to fill a a pound jar, Jelly beans it take 20 to make a oz.
 
-var jar = prompt('How many jelly beans will fill a 16oz jar? \nWhats your guess?');// to be filled with jelly beans
-var jellyBean = 20;
+var jar = prompt('How many jelly beans will fill a pound jar? \nWhats your guess?');// to be filled with jelly beans
+var jellyBean = prompt('A jelly beans is 1/16th of an oz. \nNow what is your guess?');
 var total;
 
 
-console.log ('Somthing wacky');
+console.log ('Something wacky');
 console.log(jar + '' + ' each');
