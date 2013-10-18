e2d5be8a8d247317b107ac38a5500678e58cacf0
steven creech <screech12@fullsail.edu>
2013-10-18 02:02:57 -0400

    console
    added extention to console

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,11 +6,11 @@
 
 var hours = prompt('Enter hours in take you to complete a Web page'); // hours needed per page
 console.log('Web Site');
-console.log(hours);
+console.log(hours + " " + 'hrs');
 var pages = prompt('Please enter how many pages you need.'); // How many web pages.
-console.log(pages);
+console.log(pages + " " + 'pgs');
 var totalHours = (hours * pages);//calculates total hours needed to complete.
-console.log(totalHours);
+console.log(totalHours + " " + 'hrs');
 
 alert('Total hours needed '+ totalHours + ' hrs.');
 var freePage;
