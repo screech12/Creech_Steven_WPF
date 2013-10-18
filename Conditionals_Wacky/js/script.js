a081e126dde9c73377ceadb337acb9e1acee1d90
steven creech <screech12@fullsail.edu>
2013-10-18 12:34:38 -0400

    undated
    ck previous work

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -5,7 +5,7 @@
 
 var jar = prompt('How many jelly beans will fill a pound jar? \nWhat is your guess?');// to be filled with jelly beans
 var jellyBean = prompt('A jelly beans is 1/16th of an oz. \nNow what is your guess?');
-var total =(jar + jellyBean);
+var total = (jar + jellyBean);
 alert('The correct total is 256 jelly beans.');
 if(total <= 256){// the answer is true
     }else{
