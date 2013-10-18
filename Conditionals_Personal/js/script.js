65d8c97eb317019bd889f6945c4e2460dadfff5e
steven creech <screech12@fullsail.edu>
2013-10-17 21:27:40 -0400

    adjustment
    made chages to if statements and givens

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,7 +1,7 @@
 //Steven_Creech_October_17_2013_Conditionals_Personal
 
 //Bills
-//It is the end of the month and rent and power bill is due. There is no income. What do you do first?
+//It is the end of the month and rent and power bill is due. Will income cover expenses
 var rent = prompt('Late on your rent? \nEnter your monthly rent due.'); //monthly rent now due
 var power = prompt('Late on Power bill? Please enter monthly amount.');//monthly power bill due
 var income = prompt('Please enter monthly income');//income before expenses
@@ -9,8 +9,12 @@ var total = parseInt (rent + power);//calculates info
 
 alert("Total of bills due is $" + total);
 console.log (total);//prints to the console
-if(rent){
+if(rent && power >= income){
     //if rent is due this is true
-    console.log('Talk with your Landlord');
+    console.log('Talk with your Landlord and Power company');
 }
 
+//if (income < total){
+//    //if total is less than income
+//    console.log('Talk with your landlord and Power company')
+//}
