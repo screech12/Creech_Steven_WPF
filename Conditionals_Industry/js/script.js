3b3734c84a9d41b05f423a2aa625c8d9e14ac6da
steven creech <screech12@fullsail.edu>
2013-10-17 23:33:48 -0400

    console
    page console created

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,6 +2,8 @@
 
 //determine how many hours are need to complete a website
 //Given hours per page, how many pages, total time needed
+//Web Site
 var hours = prompt('Enter hours in take you to complete a Web page'); // hours needed per page
 console.log(hours);
 var pages = prompt('Please enter how many pages you need.'); // How many web pages.
+console.log(pages);
