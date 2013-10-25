a6e6b4918cb58b23093c00fec1c337170261c23c
steven creech <screech12@fullsail.edu>
2013-10-25 00:31:05 -0400

    Givens
    setup var

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,4 +1,11 @@
 //Steven_Creech_October_24_2013_Functions_Personal
 
-//Hou many hours per week do I have available to do school work. Givens: sleeping hrs per day,work hours, personal hours,break hours
+//How many hours per week do I have available to do school work. Givens: sleeping hrs per day,work hours, personal hours,break hours
 console.log('Hours available for school');//Title
+var workHours //8
+var sleepHours //9
+var personalHours //
+var breakHours //3
+var weekHours //280
+var dailyHours //24
+var schoolHours //
