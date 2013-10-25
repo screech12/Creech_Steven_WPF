3303cf8189c3b1b9254f473936e4490cc4fe3454
steven creech <screech12@fullsail.edu>
2013-10-25 00:18:36 -0400

    setup Title
    Title of calculation and discription

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,1 +1,4 @@
 //Steven_Creech_October_24_2013_Functions_Personal
+
+//Hou many hours per week do I have available to do school work. Givens: sleeping hrs per day,work hours, personal hours,break hours
+console.log('Hours available for school');//Title
