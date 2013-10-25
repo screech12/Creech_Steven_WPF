133e44474690b50940ced43ed341c05907c62b31
steven creech <screech12@fullsail.edu>
2013-10-24 23:29:45 -0400

    setup examples
    examples to work on

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,2 +1,25 @@
 //Steven_Creech_October_24_2013_Functions_worksheet
 
+
+//Calculate the circumference of a circle.
+// Parameter(s) for function:
+//Radius of the circle
+//Return:
+//    Circumference of the circle
+//Result to print to the console:
+//    “The circumference of the circle is X”;
+
+console.log('---Circumference---');
+
+//It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function
+//    Given:
+//Victim’s weight (in pounds)
+//Parameter(s) for function:
+//Victim’s weight (in pounds)
+//Return:
+//    Number of Bee stings
+//Result to print to the console:
+//    “It takes X bee stings to kill this animal.
+
+
+console.log('---Stung!---');
