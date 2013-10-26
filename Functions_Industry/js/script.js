39e26cbc5807473448ab9752d8cb156dcf62f42b
steven creech <screech12@fullsail.edu>
2013-10-26 19:54:07 -0400

    if and else
    created statement and console print

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,10 +1,18 @@
 //  Steven_Creech_October_24_2013_Functions_Industry
 // Cost of designing websites. Determine hours needed. Cost per hour. People needed.
 
-var cost
-var hours
-var people
+var cost = 200;
+var hours = 100;
+var people = 4;
+var totalHours;
 
-var totalCost
-var totalHours
-var totalPeople
+var totalCost =((hours*cost)/people);
+console.log(totalCost);
+
+if(totalCost >= cost){
+    console.log('Not charging enough');}
+    //prints to console.
+    else{
+        console.log('Your on the right track');
+}
+
