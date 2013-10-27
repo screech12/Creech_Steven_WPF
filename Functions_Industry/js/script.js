1362bed3ea052fbf83f15a47ab3b751f679ef562
steven creech <screech12@fullsail.edu>
2013-10-26 20:51:58 -0400

    function
    if and else function

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -9,19 +9,26 @@ var totalHours;
 var totalCost =((hours*cost)/people);
 console.log(totalCost);
 
-if(totalCost >= cost){
-    console.log('Not charging enough');}
-    //prints to console.
-else if (people > hours){
-    console.log('You will not make money');
-
-}
-    else{
-        console.log('Your on the right track');
-}
+//if(totalCost >= cost){
+//    console.log('Not charging enough');}
+//    //prints to console.
+//else if (people > hours){
+//    console.log('You will not make money');
+//
+//}
+//    else{
+//        console.log('Your on the right track');
+//}
 
 function costCalculator(time,money,tech){
     return time*money/tech;
 }
 console.log(costCalculator(5,300,6));
 console.log(costCalculator(hours,cost,people));
+
+function income(totalAmount,amount){
+    var returnString ='';
+    if(totalAmont>=cost){returnString='Not charging enough'}
+    else if{returnString='Not charging enough'}
+    else{returnString='You will not make money'}
+}
